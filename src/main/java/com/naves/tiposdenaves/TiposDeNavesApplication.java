bd25df0126e102bb7d7fce8316dbc317941fd799
alejandra carvajal <alejita1234-1@hotmail.com>
2023-02-04 13:24:02 -0500

    configuracion inicial del proyecto

---
+++ src/main/java/com/naves/tiposdenaves/TiposDeNavesApplication.java
@@ -0,0 +1,13 @@
+package com.naves.tiposdenaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiposDeNavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiposDeNavesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naves/tiposdenaves/TiposDeNavesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naves.tiposdenaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiposDeNavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
